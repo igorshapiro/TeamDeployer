f5839a5943fb1fe2371b5f87aba038eaf6370633
Igor Shapiro <igor@licensario.com>
2011-11-10 15:37:24 +0200

    Initial commit

---
+++ test.rb
@@ -0,0 +1,39 @@
+require "teamcity-rest-client"
+require "yaml"
+
+class TeamcityRestClient::HttpBasicAuthentication
+  def credentials
+    [@user, @password]
+  end
+end
+
+class TeamcityRestClient::Build
+  def artifacts
+
+  end
+
+  def dump
+    puts self.inspect
+
+    #puts teamcity.authentication.get
+    #puts open(href, :http_basic_authentication => teamcity.authentication.credentials).read();
+  end
+end
+class TeamcityRestClient::Project
+  def dump
+    puts name
+
+    builds.each do |bt|
+      bt.dump
+    end
+  end
+end
+
+config = YAML.load_file("configuration.yaml")
+server = Teamcity.new(config["server"]["host"], config["server"]["port"],
+                      config["server"]["user"], config["server"]["password"])
+
+server.projects.each do |prj|
+  #puts prj.inspect
+  prj.dump
+end
